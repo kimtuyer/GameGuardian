ab848ff89cfe70dfc778265b46264f976f54b1e0
kimtuyer <kimtu@DESKTOP-Q2TQOMS>
2025-11-30 23:38:14 +0900

    패킷 캡쳐시 탐지 필요없는 패킷 종류 거르기 로직 추가 -가디언 툴이 전송한 rst 패킷 /이미 차단당한 ip 패킷 -패킷 캡쳐/탐지 스레드 분리, 캡쳐는 단일, 탐지는 멀티 스레드 -블랙리스트 추가, 앞서 차단한 ip는 패킷 캡쳐시 건너띔.

--- GameGuardian/GameGuardin.cpp
+++ GameGuardian/GameGuardin.cpp
@@ -1,15 +1,13 @@
 #include <stdio.h>
 #include <pcap.h>
 #include <time.h>
-#include <thread>
-#include <vector>
-#include <concurrent_queue.h>
-#include<Windows.h>
+#include "define.h"
 #pragma comment(lib, "wpcap")
 #pragma comment(lib, "ws2_32")
 
 #include <tchar.h>
 #include <WinSock2.h>
+using namespace std;
 
 #pragma pack(push, 1)
 typedef struct EtherHeader {
@@ -74,15 +72,28 @@ typedef struct Packet {
 	}
 
 };
+// 생산자(Producer)가 패킷 처리에 필요한 모든 '상태'를 담는 가방
+struct CaptureContext {
+	// 로컬 블랙리스트 (락 필요 없음, 생산자만 봄)
+	set<uint32_t> local_blacklist;
+	// 워커들에게서 차단 요청을 받을 피드백 큐 (스레드 안전)
+	//concurrency::concurrent_queue<uint32_t> feedback_queue;
+};
 #pragma pack(pop)
 
 
 
-using namespace std;
+const int NUM_WORKER_THREADS = 2;
+mutex m1[NUM_WORKER_THREADS];
 vector<char*> Payloadlist;
 //vector<char*> PacketBuffer;
 vector<Packet>local_buffer;
 Concurrency::concurrent_queue<Packet> PacketBuffer;
+map<uint32_t, int> IPList;
+std::vector<map<uint32_t, pair<Packet,int>>> worker_queues;
+concurrency::concurrent_queue<uint32_t> blacklist_queue;
+
+
 BOOL LoadNpcapDlls()
 {
 	_TCHAR npcap_dir[512];
@@ -240,11 +251,88 @@ void packet_Reset(const TcpHeader* pTcp,pcap_t* adhandle)
 
 }
 
-void packet_detect(pcap_t* adhandle)
+void packet_detect(const int ThreadID,pcap_t* adhandle)
 {
 
+	auto last_check_time = std::chrono::steady_clock::now();
 
 	int size = 500;
+	map<uint32_t, pair<Packet, int>> local_IPList;
+
+#ifdef __VER2__
+	while (1)
+	{
+
+		auto now = std::chrono::steady_clock::now();
+		if (chrono::duration_cast<std::chrono::seconds>(now - last_check_time).count() >= 1) {
+			last_check_time = now;
+
+			//printf("--- 1초 경과: 카운트 리셋 ---\n");
+
+			{
+				lock_guard<mutex> local_m(m1[ThreadID]);
+				if(worker_queues[ThreadID].empty()==false)
+					local_IPList.swap(worker_queues[ThreadID]);
+			}
+
+			for (auto it= local_IPList.begin(); it!=local_IPList.end();)
+			{
+				int packet_count = (*it).second.second;
+
+				/*if (packet_count < 100)
+					continue;*/
+
+				auto packet = (*it).second.first;
+
+				if (packet.m_pkt_data == nullptr || packet.m_pheader == nullptr)
+					continue;
+
+				EtherHeader* pEther = (EtherHeader*)packet.m_pkt_data;
+				IpHeader* pIpHeader = (IpHeader*)(packet.m_pkt_data + sizeof(EtherHeader));
+
+
+				int ipHeaderLen = (pIpHeader->verIhl & 0x0F) * 4;
+				TcpHeader* pTcp =
+					(TcpHeader*)(packet.m_pkt_data + sizeof(EtherHeader) + ipHeaderLen);
+
+
+				printf("%d.%d.%d.%d:%d -> %d.%d.%d.%d:%d\n",
+					pIpHeader->srcIp[0], pIpHeader->srcIp[1],
+					pIpHeader->srcIp[2], pIpHeader->srcIp[3],
+					ntohs(pTcp->srcPort),
+					pIpHeader->dstIp[0], pIpHeader->dstIp[1],
+					pIpHeader->dstIp[2], pIpHeader->dstIp[3],
+					ntohs(pTcp->dstPort)
+				);
+
+				int tcpHeaderSize = ((pTcp->data >> 4 & 0x0F) * 4);
+				char* pPayload = (char*)(packet.m_pkt_data + sizeof(EtherHeader) +
+					ipHeaderLen + tcpHeaderSize);
+
+				int Segmentsize = ntohs(pIpHeader->length) - ipHeaderLen - tcpHeaderSize;
+				printf("Segment size: %d(Frame length: %d)\n",
+					Segmentsize,
+					packet.m_pheader->len);
+
+
+				//클라->서버 ,서버->클라, 클라->서버 ACK 보내는 마지막 패킷 캡쳐
+				if (pTcp->flags == 0x010) // Flags 비트 값이 0x010 (ACK)일 경우에만 읽고 탐지
+				{
+					packet_Reset(pTcp, adhandle);
+					blacklist_queue.push((*it).first);
+				}
+				it = local_IPList.erase(it);
+				//if (Segmentsize == 0) //클라->서버 ,서버->클라, 클라->서버 ACK 보내는 마지막 패킷 캡쳐
+				//{
+
+				//	packet_Reset(pTcp, adhandle);
+
+				//}
+			}
+		}
+	}
+
+#else
 	while (1)
 	{
 		Packet packet;
@@ -256,7 +344,7 @@ void packet_detect(pcap_t* adhandle)
 
 
 
-			*/
+			*/			
 			if (packet.m_pkt_data == nullptr || packet.m_pheader == nullptr)
 				return;
 
@@ -286,6 +374,26 @@ void packet_detect(pcap_t* adhandle)
 				ntohs(pTcp->dstPort)
 			);
 
+			/*
+				sourceIP 로 각 패킷 전송자 식별해서 저장
+				해당 ip가 초당 몇개의 패킷을 전송하는지!
+			
+			*/
+			/*string sIP;
+			sIP.push_back(pIpHeader->srcIp[0]);
+			sIP.push_back(pIpHeader->srcIp[1]);
+			sIP.push_back(pIpHeader->srcIp[2]);
+			sIP.push_back(pIpHeader->srcIp[3]);
+
+			if (IPList.contains(sIP) == false)
+				IPList.insert({ sIP,1 });
+			else
+			{
+				IPList[sIP]++;
+
+				 
+
+			}*/
 			int tcpHeaderSize = ((pTcp->data >> 4 & 0x0F) * 4);
 			char* pPayload = (char*)(packet.m_pkt_data + sizeof(EtherHeader) +
 				ipHeaderLen + tcpHeaderSize);
@@ -310,29 +418,53 @@ void packet_detect(pcap_t* adhandle)
 		}
 
 	}
+#endif // __Ver2__
 
 }
 
-void packet_handler(u_char* temp1,
+void packet_handler(u_char* param,
 	const struct pcap_pkthdr* header,
 	const u_char* pkt_data)
 {
+	CaptureContext* ctx = (CaptureContext*)param;
 	EtherHeader* pEther = (EtherHeader*)pkt_data;
 	IpHeader* pIpHeader = (IpHeader*)(pkt_data + sizeof(EtherHeader));
+
+	uint32_t src_ip = *(uint32_t*)(pIpHeader->srcIp);
+	uint32_t ip{};
+
+	int ipHeaderLen = (pIpHeader->verIhl & 0x0F) * 4;
+	TcpHeader* pTcp = (TcpHeader*)(pkt_data + sizeof(EtherHeader) + ipHeaderLen);
+
+	//이미 차단된 블랙 ip 패킷은 건너띔!
+	if (ctx->local_blacklist.contains(src_ip))
+		return;
+
+	auto now = std::chrono::steady_clock::now();
+
+	while (blacklist_queue.try_pop(ip))
+	{	
+		auto last_check_time = std::chrono::steady_clock::now();
+
+		//1초이상 경과시 다음에 처리
+		if (chrono::duration_cast<std::chrono::seconds>(now - last_check_time).count() >= 1)
+		{
+			break;
+		}
+		
+		ctx->local_blacklist.insert(ip);				
+	}
+
 
 	if (pEther->type != 0x0008)
 		return;
 
 	if (pIpHeader->protocol != 6)
 		return;
-
 	
-	int ipHeaderLen = (pIpHeader->verIhl & 0x0F) * 4;
-	TcpHeader* pTcp =
-		(TcpHeader*)(pkt_data + sizeof(EtherHeader) + ipHeaderLen);
-
-	if (pTcp->flags != 0x010) // Flags 비트 값이 0x010 (ACK)일 경우에만 읽고 탐지
+	if (pTcp->flags == 0x004) //RST 패킷, 즉 툴이 보내는 종료패킷은 제외!
 		return;
+
 	//if (pIpHeader->id == 0x3412)
 	//	return; //툴 자신이 생성해 보낸 패킷은 제외!
 
@@ -340,32 +472,46 @@ void packet_handler(u_char* temp1,
 	if (ntohs(pTcp->srcPort) != 25000 &&  ntohs(pTcp->dstPort) != 25000) //ntohs(pTcp->srcPort) != 25000 &&
 		return;
 
-
 	Packet data(const_cast<pcap_pkthdr*>(header), const_cast<u_char*>(pkt_data));
 
-	/*printf("%d.%d.%d.%d:%d -> %d.%d.%d.%d:%d\n",
-		pIpHeader->srcIp[0], pIpHeader->srcIp[1],
-		pIpHeader->srcIp[2], pIpHeader->srcIp[3],
-		ntohs(pTcp->srcPort),
-		pIpHeader->dstIp[0], pIpHeader->dstIp[1],
-		pIpHeader->dstIp[2], pIpHeader->dstIp[3],
-		ntohs(pTcp->dstPort)
-	);
+	int worker_index = src_ip % NUM_WORKER_THREADS;
 
-	int tcpHeaderSize = ((pTcp->data >> 4 & 0x0F) * 4);
-	char* pPayload = (char*)(pkt_data + sizeof(EtherHeader) +
-		ipHeaderLen + tcpHeaderSize);
+	{
+		std::lock_guard<mutex> lock(m1[worker_index]);
+		if (worker_queues[worker_index].contains(src_ip))
+		{
+			//클라->서버 ,서버->클라, 클라->서버 ACK 보내는 마지막 패킷 캡쳐
+			if (pTcp->flags == 0x010) // Flags 비트 값이 0x010 (ACK)일 경우에만 패킷데이터 업데이트!
+			{
 
-	printf("Segment size: %d(Frame length: %d)\n",
-		ntohs(pIpHeader->length) - ipHeaderLen - tcpHeaderSize,
-		header->len);*/
+				worker_queues[worker_index][src_ip].first = data;
+				worker_queues[worker_index][src_ip].second++;
+			}
+			else
+			{
+				// Flags 비트 값이 0x010 (ACK) 가 아닐 경우엔 카운트만 증가
+				worker_queues[worker_index][src_ip].second++;
+			}
 
+		}
+		else
+		{
+
+			worker_queues[worker_index].insert({ src_ip ,{data,1} });
+		}
+
+	}
+
+
+#ifdef __VER2__
+#else
 	if (local_buffer.size() < 500)
 		local_buffer.push_back(data);
 
 	for (auto data : local_buffer)
 		PacketBuffer.push(data);
 	local_buffer.clear();
+#endif // __VER2__
 
 	/*char szMessage[2048] = { 0 };
 	memcpy_s(szMessage, sizeof(szMessage), pPayload,
@@ -382,7 +528,10 @@ int main()
 	pcap_t* adhandle;
 	char errbuf[PCAP_ERRBUF_SIZE];
 	Payloadlist.reserve(1000);
-
+	worker_queues.resize(NUM_WORKER_THREADS);
+	CaptureContext my_context;
+	//for (int i = 0; i < NUM_WORKER_THREADS; i++)
+	//	worker_queues
 	if (!LoadNpcapDlls())
 	{
 		fprintf(stderr, "Couldn't load Npcap\n");
@@ -486,13 +635,13 @@ int main()
 	}	/* start the capture */
 
 	////캡쳐한 패킷 버퍼에서 뽑아내 추출하는 스레드풀 생성
-	const int threadCnt = 1;//std::thread::hardware_concurrency();
+	const int threadCnt = NUM_WORKER_THREADS;//1;//std::thread::hardware_concurrency();
 	std::vector<thread> ThreadPool;
 	for (int i = 0; i < threadCnt; i++)
-		ThreadPool.push_back(thread(packet_detect, adhandle));
+		ThreadPool.push_back(thread(packet_detect, i,adhandle));
 
 
-	pcap_loop(adhandle, 0, packet_handler, NULL);
+	pcap_loop(adhandle, 0, packet_handler, (u_char*)&my_context);
 
 
 
---
+++ GameGuardian/define.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <thread>
+#include <vector>
+#include <concurrent_queue.h>
+#include<map>
+#include<string>
+#include<chrono>
+#include<mutex>
+#include<Windows.h>
+#include <queue>
+#include <set>
+#define __VER2__
